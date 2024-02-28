e512e5ef0946b6c30a949cfdeab4a4f8e828b52f
mid-kid <esteve.varela@gmail.com>
2024-02-28 22:56:19 +0100

    Dump the entirety of NameScreen_ConfirmDialog

--- source/name_screen.asm
+++ source/name_screen.asm
@@ -2,23 +2,39 @@ include "include/macros.inc"
 include "include/bank_ends.inc"
 include "include/char_main.inc"
 
-section "NameScreen_code547d", romx[$547d], bank[$14]
+section "NameScreen_ConfirmDialog", romx[$546d], bank[$14]
+NameScreen_ConfirmDialog:
+    ld a, [$cc3c]
+    call $2f5f
+    call $3056
+    call $04d2
+    xor a
+    ld [$cc40], a
     ;ld bc, $0305
     ;ld de, $0d07
     ld bc, $0205
     ld de, $0f07
-
-section "NameScreen_code5491", romx[$5491], bank[$14]
+    farcall $10, $6901
+    ldh a, [$83]
+    push af
+    ld a, $01
+    ldh [$83], a
+    ldh [$4f], a
+    xor a
     ;ld bc, $0406
     ;ld de, $0b05
     ld bc, $0306
     ld de, $0d05
-
-section "NameScreen_code54a0", romx[$54a0], bank[$14]
+    farcall $15, $6ad3
+    pop af
+    ldh [$83], a
+    ldh [$4f], a
     ;ld bc, $0507
     ld bc, $0407
     ld hl, NameScreen_ConfirmText
     call $3353
+    call $54d8
+    ret
 
 section "NameScreen_ConfirmText", romx[$56ee], bank[$14]
 ;NameScreen_ConfirmText:
