45809470e3b5cd7a5595cd773b16718db09b3651
Zach Robinson <pokecrystal-mobile@outlook.com>
2024-03-03 23:49:07 -0500

    Filled out the Unit Creation menu strings and code

--- source/unit_status.asm
+++ source/unit_status.asm
@@ -1,9 +1,79 @@
 include "include/macros.inc"
-include "include/char_main.inc"
+include "include/bank_ends.inc"
 include "include/char_unit.inc"
+include "include/char_main.inc"
+
+setcharmap unit
+section "UnitCreation_Menu", romx[$5a0d], bank[$0b]
+UnitCreation_code5a0d:
+    ld a, $07
+    call $7a9d
+    ld a, [$c9ca]
+    and a
+    jr nz, $5a1d
+
+    ld hl, UnitCreation_Create
+    jr $5a20
+
+UnitCreation_code5a1d:
+    ld hl, UnitCreation_Call
+
+UnitCreation_code5a20:
+    lb bc, 1, 32
+    call TextPut
+    ld hl, UnitCreation_Gold
+    lb bc, 4, 35
+    call TextPut
+    ld hl, UnitCreation_Material
+    lb bc, 12, 35
+    call TextPut
+    ld hl, UnitCreation_Movement
+    lb bc, 1, 37
+    call TextPut
+    ld hl, UnitCreation_Fuel
+    lb bc, 1, 38
+    call TextPut
+    ld hl, UnitCreation_Separator
+    lb bc, 1, 36
+    call TextPut
+    call $352e
+    ret
+
+UnitCreation_Create:
+    ;text "どのユニットをせいさんしますか"
+    text "BUILD WHICH UNIT?"
+    done
+
+UnitCreation_Call:
+    ;text "どのユニットをよびますか"
+    text "CALL WHICH UNIT?"
+    done
+
+UnitCreation_Gold:
+    db $b1, "-   00"
+    done
+
+UnitCreation_Material:
+    db $b2, "-"
+    done
+
+UnitCreation_Movement:
+    db $af, "  :"
+    done
+
+UnitCreation_Fuel:
+    db $b0, "  :"
+    done
+
+    section_end $5a9c
+
+section fragment "bank0b_end", romx[bank0b_end_addr], bank[$0b]
+UnitCreation_Separator:
+    text "___________________"
+    done
 
 setcharmap main
-section "Unit_Status_Types", romx[$5e49], bank[$25]
+section "UnitStatus_Types", romx[$5e49], bank[$25]
     ;text "そうこうユニット "
     text "ARMORED  "
     done
@@ -14,20 +84,20 @@ section "Unit_Status_Types", romx[$5e49], bank[$25]
     text "AIR      "
     done
     ;text "うみユニット   "
-    text "NAVAL    "
+    text "SEA      "
     done
     ;text "せんすいかん   "
     text "SUB      "
     done
 
-section "Unit_Status_Header", romx[$60d8], bank[$25]
+section "UnitStatus_Header", romx[$60d8], bank[$25]
     ;text "ユニットについての くわしい"
     ;line "じょうほうを みることができます。"
     text "VIEW DETAILED UNIT"
     line "INFORMATION。 "
     done
 
-section "Unit_Status_Strings", romx[$6349], bank[$25]
+section "UnitStatus_Strings", romx[$6349], bank[$25]
     ;text "いどうりょく    /"
     text "MOVE      /"
     done
@@ -47,7 +117,7 @@ section "Unit_Status_Strings", romx[$6349], bank[$25]
     coord_text 2, 16, "ほきゅう•ほじゅう"
     ;coord_text 2, 16, "RESUPPLY•REPAIR"
 
-section "Unit_Status_None", romx[$66fe], bank[$25]
+section "UnitStatus_None", romx[$66fe], bank[$25]
     text "なし"
     ;text "NONE"
     done
@@ -55,7 +125,7 @@ section "Unit_Status_None", romx[$66fe], bank[$25]
     text "N/A  "
     done
 
-section "25:6ad2", romx[$6ad0], bank[$25] ; Unit Creation
+section "UnitStatus_Costs", romx[$6ad0], bank[$25]
     ;coord_text 1, 3, "せいさんしょうひしきん"
     coord_text 1, 3, "GOLD COST  "
     ;coord_text 1, 4, "せいさんしょうひしざい"
