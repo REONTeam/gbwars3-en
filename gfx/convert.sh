31e90fa2169efb582b83eeae9e2f72fc8638f31d
mid-kid <esteve.varela@gmail.com>
2024-03-06 08:14:36 +0100

    Improve conversion script somewhat
    The functionality of this one is fairly rigid, so it might not be
    applicable for use with any graphics (case in point,
    system_messages.png, which would require more transformations to be
    readable).  However, it provides an acceptable baseline to document how
    things were extracted.

--- gfx/convert.sh
+++ gfx/convert.sh
@@ -4,6 +4,23 @@ set -eu
 bpp2png=../tools/bpp2png
 test -f "$bpp2png" || make -C ../tools bpp2png
 
+dump() {
+    bank="$(printf %d "$1")"
+    addr="$(printf %d "$2")"
+    size="$(printf %d "$3")"
+    skip="$(($bank * 0x4000 + $addr % 0x4000))"
+    dd if=../baserom.gbc bs=1 skip="$skip" count="$size" 2> /dev/null
+}
+
+conv() {
+    dest="$1"; shift
+    "$bpp2png" "$@" "$dest" "${dest%.*}.png"
+}
+
 set -x
-"$bpp2png" -w 32 action_menu.orig.2bpp action_menu.orig.png
-"$bpp2png" -w 64 system_messages.orig.2bpp system_messages.orig.png
+
+dump 0x11 0x4e88 0x0f80 > action_menu.orig.2bpp
+conv action_menu.orig.2bpp -w 32
+
+dump 0x11 0x5e10 0x18a0 > system_messages.orig.2bpp
+conv system_messages.orig.2bpp -w 64
